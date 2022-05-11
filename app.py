054c1fc13015f751cdcb40420c7796c71b2278a2
Lorena Sanchez <hector2603@users.noreply.github.com>
2022-05-11 09:03:25 -0500

    add methods for create and update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import json
-from flask import Flask
+from flask import Flask , request
 
 app = Flask(__name__)
 
@@ -34,6 +34,53 @@ def get_people():
 
   return json.dumps(json_data)
 
+@app.route('/create', methods=['POST'])
+def add_person():
+  conn = None
+  cursor = None
+  json_data = request.get_json(force=True)
+  _name = json_data['name']
+  _lastName = json_data['lastName']
+  sql = "INSERT INTO person (name, last_name)VALUES(%s, %s)"
+  data = (_name, _lastName,)
+  conn = mysql.connector.connect(
+    host="mysqldb",
+    user="root",
+    password="p@ssw0rd1",
+    database="docker"
+  )
+  cursor = conn.cursor()
+  cursor.execute(sql, data)
+  conn.commit()
+  cursor.close() 
+  conn.close()
+  return "person created successfully"
+
+
+@app.route('/update', methods=['POST'])
+def update_person():
+	conn = None
+	cursor = None
+	json_data = request.get_json(force=True)
+	_name = json_data['name']
+	_lastName = json_data['lastName']
+	_id = json_data['id']
+	sql = "UPDATE person SET name=%s, last_name=%s WHERE id=%s"
+	data = (_name, _lastName, _id,)
+	conn = mysql.connector.connect(
+    host="mysqldb",
+    user="root",
+    password="p@ssw0rd1",
+    database="docker"
+  )
+	cursor = conn.cursor()
+	cursor.execute(sql, data)
+	conn.commit()
+	cursor.close() 
+	conn.close()
+	return "person updated successfully"
+
+
 @app.route('/initdb')
 def db_init():
   mydb = mysql.connector.connect(
@@ -54,10 +101,7 @@ def db_init():
   )
   cursor = mydb.cursor()
 
-  cursor.execute("CREATE TABLE IF NOT EXISTS person (name VARCHAR(255), last_name VARCHAR(255))")
-  cursor.execute("INSERT INTO person VALUES ('Hector', 'Ocampo')")
-  cursor.execute("INSERT INTO person VALUES ('Andrea', 'Fernandez')")
-  cursor.execute("INSERT INTO person VALUES ('Javier', 'Rosero')")
+  cursor.execute("CREATE TABLE IF NOT EXISTS person (id MEDIUMINT NOT NULL AUTO_INCREMENT ,name VARCHAR(255), last_name VARCHAR(255), PRIMARY KEY (id))")
   mydb.commit()
 
   cursor.close()
